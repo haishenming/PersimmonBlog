b1d62eda451044ff763e3c304606663f99212d92
haishenming <haishenmingx@gmail.com>
2018-05-01 13:09:03 +0800

    发布文章页面

--- app/art/views.py
+++ app/art/views.py
@@ -5,13 +5,13 @@ from . import art
 # 发布文章
 @art.route("/add/", methods=["GET", "POST"])
 def add():
-    return render_template("art/art_add.html")
+    return render_template("art/art_add.html", title="添加文章")
 
 
 # 编辑文章
 @art.route("/edit/<int:id>", methods=["GET", "POST"])
 def edit(id):
-    return render_template("art/art_edit.html")
+    return render_template("art/art_edit.html", title="编辑文章")
 
 
 # 删除文章
@@ -23,4 +23,4 @@ def dele(id):
 # 文章列表
 @art.route("/list/", methods=["GET"])
 def list():
-    return render_template("art/art_list.html")
+    return render_template("art/art_list.html", title="文章列表")
